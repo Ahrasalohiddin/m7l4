4007ea3a1d2c70ac3c42aaab6ba701240172f4fc
Alahadzin <140735314+Ahrasalohiddin@users.noreply.github.com>
2024-09-29 16:19:35 +0500

    Add files via upload

---
+++ test_registration.py
@@ -0,0 +1,44 @@
+import pytest
+import sqlite3
+import os
+from registration.registration import create_db, add_user, authenticate_user, display_users
+
+@pytest.fixture(scope="module")
+def setup_database():
+    """Фикстура для настройки базы данных перед тестами и её очистки после."""
+    create_db()
+    yield
+    try:
+        os.remove('users.db')
+    except PermissionError:
+        pass
+
+@pytest.fixture
+def connection():
+    """Фикстура для получения соединения с базой данных и его закрытия после теста."""
+    conn = sqlite3.connect('users.db')
+    yield conn
+    conn.close()
+
+
+def test_create_db(setup_database, connection):
+    """Тест создания базы данных и таблицы пользователей."""
+    cursor = connection.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='users';")
+    table_exists = cursor.fetchone()
+    assert table_exists, "Таблица 'users' должна существовать в базе данных."
+
+def test_add_new_user(setup_database, connection):
+    """Тест добавления нового пользователя."""
+    add_user('testuser', 'testuser@example.com', 'password123')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE username='testuser';")
+    user = cursor.fetchone()
+    assert user, "Пользователь должен быть добавлен в базу данных."
+
+def test_authenyicate_user():
+    add_user('testuser', 'testures@mail.ru', 'pasword123')
+    assert authenticate_user('testuser', 'pasword123') == True
+
+def test_authenticate_nonexistent_user(setup_database):
+    assert authenticate_user('nonexistentuser', 'password1234567') == False       
